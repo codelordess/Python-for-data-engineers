382515dce9a7c541ac67a928c7ed52965882c49d
Hephzibah56 <hephzibahemereole@gmail.com>
2026-01-03 23:56:38 +0000

    working with variables and strings Day3

---
+++ Week 1/math.py
@@ -0,0 +1,10 @@
+import math
+#Formatting Numbers
+x = 10
+x = x + 3
+x +=3
+
+print (round(2.9))
+print(abs(-2.9))
+
+#print(math.ceil(2.2))
---
+++ Week 1/variables.py
@@ -0,0 +1,35 @@
+sudents_count = 1000
+rating = 4.99
+is_published = True
+course = "Python Programming"
+
+#printing the indexes of the given string
+print(len(course))
+print(course[0])
+print(course[0:3])
+print(course[0])
+print(course[-1])
+print(course[0:])
+print(course[:3])
+
+
+#Backslashing a given string
+backslash_course= "Python \\ Programming"
+print(backslash_course)
+
+
+#Formatting a given string
+first = "Heph"
+last = "Emere"
+full = f" {len(first)} {2 + 2}" 
+print(full)
+
+course = "Python Programming"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.rstrip())
+print(course.find("Pro"))
+print(course.replace("p", "j"))
+print("pro" in course)
+print("swift" not in course)
