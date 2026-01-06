605a50baeda18c8c77b4c612982ed5487577d601
Hephzibah56 <hephzibahemereole@gmail.com>
2026-01-06 23:53:13 +0000

    Day 6- all about loops and functions

---
+++ Week 1/function.py
@@ -0,0 +1,36 @@
+def greet():
+    print("Hi there")
+    print("Welcome aboard")
+greet()
+
+
+
+def greet(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome aboard")
+
+greet("Hephzibah", "Emereole")
+
+def greet (name):
+    print(f"Hi {name}")
+
+
+def get_greeting(name):
+    return f"Hi {name}"
+
+message = get_greeting("Mosh")
+file = open("content.txt", "w")
+file.write(message)
+print (f"Contents have been written to a file {message}")
+
+
+def increment(number, by):
+    return number + by
+
+def greet(name):
+    print(f"Hi {name}")
+
+print(greet("Mosh"))
+
+print(increment (2, 1))
+print(increment (number=2, by=1))
--- Week 1/loops.py
+++ Week 1/loops.py
@@ -1,8 +1,55 @@
 #for loop
 
+for number in range (1, 10, 2):
+    print ("Attempt", number, number * ".")
+
 count = 0
 for number in range (1, 10):
     if number % 2 == 0:
         count += 1
         print (number)
+#formatting a string
 print(f"We have {count} even numbers ")
+
+sucessful = False
+for number in range(3):
+    print("Attempt")
+    if sucessful:
+        print("Successful")
+        break
+    else:
+        print("Attempted 3 times and failed")
+
+
+#Nested for loops
+for x in range(5):
+    for y in range(3):
+        print(f"({x}, {y})")
+
+
+#Type in python
+
+print(type(5))
+print(type(range(5)))
+
+#iterable string
+for x in "PYTHON":
+    print(x)
+
+#iterable list
+for x in [1,2,3,4]:
+    print(x)
+
+#iterable custom object. we define this object at the point of calling the class
+shopping_cart = ""
+for item in shopping_cart:
+    print(item)
+
+command = ""
+while command != "quit" and command != "QUIT":
+    command = input(">")
+    print("ECHO", command)
+
+
+
+
