2450433ce09a474c3053aeb7dec86697b54d4b41
Hephzibah56 <hephzibahemereole@gmail.com>
2026-01-12 23:52:30 +0000

    learned about stack and had a weekend of coding assessment

---
+++ Weeks/Week 2/stack.py
@@ -0,0 +1,12 @@
+# Create an empty list to represent a browsing session
+browsing_session = []
+browsing_session.append("youtube.com")
+browsing_session.append("google.com")
+browsing_session.append("twitter.com")
+print(browsing_session)
+
+last = browsing_session.pop()
+print("Last visited site:", last)
+print(browsing_session)
+print("Number of sites visited:", len(browsing_session))
+print("redirecting to", browsing_session[-1])
